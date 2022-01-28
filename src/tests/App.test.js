924ed96bcc1995341c78d49b9b7388350b383616
Alex Silva <alexcssilva@gmail.com>
2022-01-28 11:17:44 -0300

    Desafio 01 concluido

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,2 +1,32 @@
-test('', () => {});
-// commit inicial
+import React from 'react';
+import { render, screen } from "@testing-library/react";
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste se o topo da aplicação contém um conjunto fixo de links.', () => {
+  it('O primeiro link deve possuir o texto Home.', () => {
+    renderWithRouter(<App />);
+    // acessar os elementos da tela
+
+    const home = screen.getByText(/home/i);
+    // interagir com os elementos (se for necessário)
+    expect(home).toBeInTheDocument();
+    // fazer os teste
+  });
+
+  it('O segundo link deve possuir o texto About', () => {
+    renderWithRouter(<App />);
+    // acessar os elementos da tela
+    const home = screen.getByText(/about/i);
+    // fazer os teste
+    expect(home).toBeInTheDocument();
+  });
+
+  it('O terceiro link deve possuir o texto Favorite Pokémons', () => {
+    renderWithRouter(<App />);
+    // acessar os elementos da tela
+    const home = screen.getByText(/Favorite Pokémons/i);
+    // fazer os teste
+    expect(home).toBeInTheDocument();
+  });
+});
